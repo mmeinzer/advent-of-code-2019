6f29c9fcd5349a38833504b657c668562b34b126
Matt Meinzer <meinzer.matthew@gmail.com>
2019-12-04 22:45:30 -0600

    Day 4 completed

---
+++ lib/advent/four.ex
@@ -0,0 +1,48 @@
+defmodule Advent.Four do
+  def one do
+    [first, last] = read_input()
+
+    first..last
+    |> Stream.filter(&is_increasing/1)
+    |> Stream.filter(&has_double/1)
+    |> Enum.count()
+  end
+
+  def two do
+    [first, last] = read_input()
+
+    first..last
+    |> Stream.filter(&is_increasing/1)
+    |> Stream.filter(&has_exact_double/1)
+    |> Enum.count()
+  end
+
+  defp is_increasing(num) do
+    original = Integer.to_string(num)
+
+    sorted =
+      original
+      |> String.split("", trim: true)
+      |> Enum.sort()
+      |> Enum.join()
+
+    sorted == original
+  end
+
+  defp has_double(num) do
+    Integer.to_string(num)
+    |> String.match?(~r/(\d)\1/)
+  end
+
+  defp has_exact_double(num) do
+    Integer.to_string(num)
+    |> String.match?(~r/((^)|(.))((?(3)(?!\1).|.))\4(?!\4)/)
+  end
+
+  defp read_input() do
+    {:ok, bin} = File.read("./lib/inputs/four.txt")
+
+    String.split(bin, ["-", "\n"], trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+end
